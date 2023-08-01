3c19815132897a5c021a27e2750ac270ae7dc14c
Rakesh Kumar Tunga <rt21aah@herts.ac.uk>
2023-08-01 14:14:25 +0100

    ansible sample file

---
+++ 02-outputs/output.tf
@@ -0,0 +1,11 @@
+output "sample" {
+  value = "Hello World"
+}
+
+# any string in terraform need to be quoted with double quotes and single quotes are not supported
+
+output "sample1" {
+  value = 1000
+}
+
+# numbers and booleans (true or false)
