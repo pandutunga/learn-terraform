6d7d2667340d5def4efd08c9f3df0fe9e64bba4e
Rakesh Kumar Tunga <rt21aah@herts.ac.uk>
2023-08-01 14:47:22 +0100

    ansible sample file

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -17,3 +17,54 @@ output "sample1" {
 output "sample-ext"{
   value = "value of sample - ${var.sample}"
 }
+
+# variable data types
+# string
+# number
+# boolean
+
+# variable types
+# In ansible - 1. Plain key , value; 2. List; 3. Map/Dict
+# In Terraform - 1. Plain , 2. List, 3. Map
+
+## plain
+variable "course" {
+  default = "Devops Training"
+}
+
+## List
+variable "courses" {
+  default = [
+  "Devops"
+  "AWS"
+  "Python"
+  ]
+}
+
+## MAp
+varibale "course_details" {
+ default = {
+   Devops = {
+     name = "DevOps"
+     timing = "10 am"
+     duration = 90
+   }
+   AWS = {
+     name = "AWS"
+     timing = "11 am"
+     duration = 30
+   }
+ }
+}
+
+output "course" {
+  value = var.course
+}
+
+output "courses" {
+  value = var.courses
+}
+
+output "course_details" {
+  value = var.course_details
+}
