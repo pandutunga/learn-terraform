2be9bfab838f1f30c49f87af845b8ff311d93ac0
Rakesh Kumar Tunga <rt21aah@herts.ac.uk>
2023-08-05 22:32:40 +0100

    ansible sample file

---
+++ 08-provisioner/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-04017b2d4af08c5eb"]
+
+  tags = {
+    Name = "Hello World"
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/pandutunga/roboshop-ansible mail.yml -e env=dev -e role_name=frontend"
+    ]
+  }
+
+}
+
+  data "aws_ami" "example" {
+    owners      = ["973714476881"]
+    most_recent = true
+    name_regex  = "Centos-8-DevOps-Practice"
+}
