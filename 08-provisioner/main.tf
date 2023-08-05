8c943d9907b0989650759a2a614f1256f891e938
Rakesh Kumar Tunga <rt21aah@herts.ac.uk>
2023-08-05 22:46:11 +0100

    ansible sample file

--- 08-provisioner/main.tf
+++ 08-provisioner/main.tf
@@ -18,13 +18,11 @@ resource "aws_instance" "web" {
 
     inline = [
       "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/pandutunga/roboshop-ansible mail.yml -e env=dev -e role_name=frontend"
+      "ansible-pull -i localhost, -U https://github.com/pandutunga/roboshop-ansible main.yml -e env=dev -e role_name=frontend"
     ]
   }
 
 }
-
-
 
   data "aws_ami" "example" {
     owners      = ["973714476881"]
