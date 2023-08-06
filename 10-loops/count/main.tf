a2976dbcee18f8ea5f5d4d388b29289f8e9a780e
Rakesh Kumar Tunga <rt21aah@herts.ac.uk>
2023-08-06 23:10:41 +0100

    ansible sample file

---
+++ 10-loops/count/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "web" {
+  #count         = 3
+  count         = length(var.instances)
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    #Name = "Hello World-${count.index}"
+    #Name = var.instances[count.index]
+    Name = element(var.instances, count.index)
+  }
+}
+
+
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+variable "instances" {
+  default = ["frontend", "catalogue", "cart"]
+}
